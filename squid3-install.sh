ed44e15adf53e6bb293cf127c417130fde15f0e0
FVIA <130783224+fviatool@users.noreply.github.com>
2024-04-19 04:01:19 +0700

    Update squid3-install.sh

--- squid3-install.sh
+++ squid3-install.sh
@@ -10,46 +10,18 @@ chmod 755 /usr/local/bin/squid-uninstall
 /usr/bin/wget -q --no-check-certificate -O /usr/local/bin/squid-add-user https://raw.githubusercontent.com/fviatool/squid-proxy/main/squid-add-user.sh
 chmod 755 /usr/local/bin/squid-add-user
 
-# Kiểm tra xem script bm-find-os đã được tải xuống chưa
-if [ ! -f /usr/local/bin/bm-find-os ]; then
-    echo "/usr/local/bin/bm-find-os không tìm thấy"
-    exit 1
+# Cài đặt Squid Proxy
+/usr/bin/apt update > /dev/null 2>&1
+/usr/bin/apt -y install squid > /dev/null 2>&1
+mv /etc/squid/squid.conf /etc/squid/squid.conf.bak
+/usr/bin/touch /etc/squid/blacklist.acl
+/usr/bin/wget -q --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/fviatool/squid-proxy/main/ubuntu-2204.conf
+if [ -f /sbin/iptables ]; then
+    /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
+    /sbin/iptables-save
 fi
-
-# Lấy thông tin hệ điều hành
-BM_OS=$(/usr/local/bin/bm-find-os)
-
-# Cài đặt Squid Proxy tùy theo hệ điều hành
-if [ $BM_OS == "ubuntu" ]; then
-    /usr/bin/apt update > /dev/null 2>&1
-    /usr/bin/apt -y install squid > /dev/null 2>&1
-    mv /etc/squid/squid.conf /etc/squid/squid.conf.bak
-    /usr/bin/touch /etc/squid/blacklist.acl
-    /usr/bin/wget -q --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/fviatool/squid-proxy/main/ubuntu-2204.conf
-    if [ -f /sbin/iptables ]; then
-        /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
-        /sbin/iptables-save
-    fi
-    systemctl restart squid
-    systemctl enable squid
-elif [ $BM_OS == "debian" ] || [ $BM_OS == "debian12" ]; then
-    /bin/rm -rf /etc/squid
-    /usr/bin/apt update > /dev/null 2>&1
-    /usr/bin/apt -y install squid > /dev/null 2>&1
-    /usr/bin/wget -q --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/fviatool/squid-proxy/main/squid-debian.conf
-    if [ -f /sbin/iptables ]; then
-        /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
-        /sbin/iptables-save
-    fi
-    systemctl restart squid
-    systemctl enable squid
-elif [ $BM_OS == "centos7" ] || [ $BM_OS == "centos8" ] || [ $BM_OS == "almalinux8" ] || [ $BM_OS == "almalinux9" ]; then
-    yum install squid -y
-    mv /etc/squid/squid.conf /etc/squid/squid.conf.bak
-    /usr/bin/wget -q --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/fviatool/squid-proxy/main/squid-centos.conf
-    systemctl restart squid
-    systemctl enable squid
-fi
+systemctl restart squid
+systemctl enable squid
 
 GREEN='\033[0;32m'
 CYAN='\033[0;36m'
